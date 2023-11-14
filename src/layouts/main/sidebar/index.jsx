7e34d7282738aa084681af6a9a2374adcc2b4252
Seymu1R <seymuram@code.edu.az>
2023-11-15 00:03:02 +0400

    adding init

---
+++ src/layouts/main/sidebar/index.jsx
@@ -0,0 +1,10 @@
+import Logo from "./logo";
+
+export default function SideBar() {
+    return (
+        <aside className="w-[275px] px-2 min-h-screen" >
+            <Logo />
+            sidebar
+        </aside>
+    )
+}
