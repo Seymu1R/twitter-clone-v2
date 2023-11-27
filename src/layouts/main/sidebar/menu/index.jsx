6e3f985b032b9388030088f3756b0dc5150d7238
Seymu1R <seymuram@code.edu.az>
2023-11-27 23:37:29 +0400

    Side bar changes

--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import classNames from "classnames";
 import { mainMenu } from "../../../../utils/const";
+import Button from "../../../../components/button";
 
 
 export default function Menu() {
@@ -33,8 +34,29 @@ export default function Menu() {
                 </NavLink>
             ))}
 
+            <button className="py-[3px] block group">
+                <div
+                    className="p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]">
+                    <div className="w-[26.25px] h-[26.25px] relative">
+                        <svg width={26.25} height={26.25} viewBox="0 0 24 24" aria-hidden="true" className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1nao33i r-lwhw9o r-cnnz9e">
+                            <path
+                                fill="#e7e9ea"
+                                d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 
+                                1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z" />
+                        </svg>
+                    </div>
+                    <div className="pr-4 text-xl">
+                        More
+                    </div>
+                </div>
+
+            </button>
+            <div className="py-4 w-[90%]" >
+                <Button size={"large"} >Send </Button>
+            </div>
 
 
-        </nav>
+
+        </nav >
     )
 }
